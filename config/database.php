3460e90b47f4b649187cc31d427bab1359e4bb6c
Gatard Florian <b01f60002@TREM.BOD>
2025-11-13 10:20:05 +0100

    nothing

--- config/database.php
+++ config/database.php
@@ -1,9 +1,15 @@
 <?php
 declare(strict_types=1);
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 if (!defined('DB_HOST')) {
     require_once __DIR__ . '/config.php';
 }
+
+
 
 /**
  * Classe de gestion de la connexion PDO
